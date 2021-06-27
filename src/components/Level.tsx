7b5516353f82a9779a1eaba6c32e6ad0ad9f0fc0
copperkraft <copperkraft@gmail.com>
2021-06-27 23:19:17 +0300

    Added level converter basic

--- src/components/Level.tsx
+++ src/components/Level.tsx
@@ -1,18 +1,32 @@
-import React, { useRef } from 'react';
+import React, {
+  useEffect, useMemo, useRef, useState,
+} from 'react';
 
 import { Mesh, MirroredRepeatWrapping } from 'three';
-import { Plane, useTexture } from '@react-three/drei';
+import { Plane, useTexture, Text } from '@react-three/drei';
 import mapUrl from '../textures/gravel/gravel_ground_01_diff_1k.jpg';
 import displacementUrl from '../textures/gravel/gravel_ground_01_disp_1k.jpg';
 import normalUrl from '../textures/gravel/gravel_ground_01_nor_1k.jpg';
 import roughUrl from '../textures/gravel/gravel_ground_01_rough_1k.jpg';
 import aoUrl from '../textures/gravel/gravel_ground_01_ao_1k.jpg';
+import { mapImageToLevel } from '../utils/levelConverter';
 
 interface LevelProps {
   size: [number, number];
 }
 
-export const Level: React.FC<LevelProps> = ({ size }: LevelProps) => {
+export const Level: React.FC<LevelProps> = () => {
+  const [level, setLevel] = useState<string[][]>([]);
+
+  useEffect(() => {
+    mapImageToLevel().then((loadedLevel) => setLevel(loadedLevel));
+  });
+
+  const size = useMemo<[number, number]>(() => ([
+    level.length,
+    level[0]?.length || 0,
+  ]), [level]);
+
   const terrain = useRef<Mesh>(null!);
   const [
     map,
@@ -41,24 +55,29 @@ export const Level: React.FC<LevelProps> = ({ size }: LevelProps) => {
   });
 
   return (
-    <Plane
-      visible
-      position={[0, 0, 0]}
-      rotation={[-Math.PI / 2, 0, 0]}
-      ref={terrain}
-      args={size}
-    >
-      <meshStandardMaterial
-        map={map}
-        roughnessMap={rough}
-        normalMap={normal}
-        aoMap={ao}
-        attach="material"
-        roughness={1}
-        metalness={0}
-        opacity={1}
-        transparent
-      />
-    </Plane>
+    <>
+      <Plane
+        visible
+        position={[0, 0, 0]}
+        rotation={[-Math.PI / 2, 0, 0]}
+        ref={terrain}
+        args={size}
+      >
+        <meshStandardMaterial
+          map={map}
+          roughnessMap={rough}
+          normalMap={normal}
+          aoMap={ao}
+          attach="material"
+          roughness={1}
+          metalness={0}
+          opacity={1}
+          transparent
+        />
+      </Plane>
+      <Text>
+        { level.map((row) => row.join(',')).join('\n') }
+      </Text>
+    </>
   );
 };
