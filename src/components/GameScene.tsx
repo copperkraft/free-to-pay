6b3da0d24c4e38918314db2dd7f87f9c79bcbf7e
Uladzimir Yakushkin <copperkraft@gmail.com>
2021-06-30 01:52:43 +0300

    Feature/level converter (#1)
    Add 3d level and map converter

--- src/components/GameScene.tsx
+++ src/components/GameScene.tsx
@@ -8,8 +8,6 @@ import { Level } from './Level';
 
 interface GameSceneProps { }
 
-const levelSize: [number, number] = [40, 40];
-
 export const GameScene: React.FC<GameSceneProps> = ({
   children,
 }: PropsWithChildren<GameSceneProps>) => {
@@ -51,12 +49,8 @@ export const GameScene: React.FC<GameSceneProps> = ({
 
   return (
     <>
-      <Level
-        size={levelSize}
-        onTerrainPointerDown={onTerrainPointerDown}
-        onTerrainPointerMove={onTerrainPointerMove}
-      />
-      <pointLight color="white" intensity={2} position={[10, 10, 5]} />
+      <Level />
+      <pointLight color="white" intensity={2} position={[10, 20, 5]} />
       <ambientLight color="yellow" intensity={0.2} />
       <Character moveTarget={moveTarget} />
       {children}
--- src/components/Level.tsx
+++ src/components/Level.tsx
@@ -1,79 +1,25 @@
-import React, { useRef } from 'react';
+import React, {
+  useEffect, useMemo, useState,
+} from 'react';
+import { mapImageToLevel } from '../utils/levelConverter';
+import { Farm } from '../models/Farm';
 
-import { Mesh, MirroredRepeatWrapping, Vector3 } from 'three';
-import { Plane, useTexture } from '@react-three/drei';
-import { ThreeEvent } from '@react-three/fiber';
-import mapUrl from '../textures/gravel/gravel_ground_01_diff_1k.jpg';
-import displacementUrl from '../textures/gravel/gravel_ground_01_disp_1k.jpg';
-import normalUrl from '../textures/gravel/gravel_ground_01_nor_1k.jpg';
-import roughUrl from '../textures/gravel/gravel_ground_01_rough_1k.jpg';
-import aoUrl from '../textures/gravel/gravel_ground_01_ao_1k.jpg';
+export const Level: React.FC = () => {
+  const [level, setLevel] = useState<string[][]>([]);
 
-interface LevelProps {
-  size: [number, number];
-  onTerrainPointerDown: (position: Vector3) => void
-  onTerrainPointerMove: (position: Vector3) => void;
-}
-
-export const Level: React.FC<LevelProps> = ({
-  size,
-  onTerrainPointerDown,
-  onTerrainPointerMove,
-}: LevelProps) => {
-  const terrain = useRef<Mesh>(null!);
-  const [
-    map,
-    displacement,
-    normal,
-    rough,
-    ao,
-  ] = useTexture([
-    mapUrl,
-    displacementUrl,
-    normalUrl,
-    roughUrl,
-    aoUrl,
-  ]);
-
-  [map, displacement, normal, rough, ao].forEach((texture) => {
-    if (texture.wrapS === MirroredRepeatWrapping) {
-      return;
-    }
-    /* eslint-disable no-param-reassign */
-    texture.wrapS = MirroredRepeatWrapping;
-    texture.wrapT = MirroredRepeatWrapping;
-    texture.repeat.set(6, 6);
-    texture.anisotropy = 4;
-    /* eslint-enable no-param-reassign */
+  useEffect(() => {
+    mapImageToLevel().then((loadedLevel) => setLevel(loadedLevel));
   });
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const size = useMemo<[number, number]>(() => ([
+    level.length,
+    level[0]?.length || 0,
+  ]), [level]);
+
   return (
-    <Plane
-      visible
-      position={[0, 0, 0]}
-      rotation={[-Math.PI / 2, 0, 0]}
-      ref={terrain}
-      args={size}
-      onPointerDown={(event: ThreeEvent<PointerEvent>) => {
-        if (event.button === 0) {
-          onTerrainPointerDown(new Vector3(...event.point.toArray()));
-        }
-      }}
-      onPointerMove={(event: ThreeEvent<PointerEvent>) => {
-        onTerrainPointerMove(new Vector3(...event.point.toArray()));
-      }}
-    >
-      <meshStandardMaterial
-        map={map}
-        roughnessMap={rough}
-        normalMap={normal}
-        aoMap={ao}
-        attach="material"
-        roughness={1}
-        metalness={0}
-        opacity={1}
-        transparent
-      />
-    </Plane>
+    <group>
+      <Farm />
+    </group>
   );
 };
